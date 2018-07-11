ce3989c6146df7942306435a1eadd08962f797d1
Florian Brucker <florian.brucker@it.karlsruhe.de>
2018-07-11 13:08:47 +0200

    Wrappers for package dicts and package extras

--- ctxmgr.py
+++ ctxmgr.py
@@ -32,9 +32,8 @@ class Importer(object):
                     'ckanext-importer-package-eid': lib.solr_escape(eid),
                 },
             )
-            print('Packages for EID {}: {}'.format(eid, pkg_dicts))
             if not pkg_dicts:
-                pkg_dict = lib.create_package(
+                self._pkg_dict = lib.create_package(
                     self._outer._api,
                     _PACKAGE_NAME_PREFIX,
                     owner_org=self._outer.default_owner_org,
@@ -45,40 +44,95 @@ class Importer(object):
                          'value': eid},
                     ],
                 )
-                print('Created package {} for EID {}'.format(pkg_dict['id'], eid))
+                print('Created package {} for EID {}'.format(self._pkg_dict['id'], eid))
             elif len(pkg_dicts) > 1:
                 raise ValueError('Multiple packages for EID {}'.format(eid))
             else:
-                pkg_dict = pkg_dicts[0]
+                self._pkg_dict = pkg_dicts[0]
+                print('Using existing package {} for EID {}'.format(self._pkg_dict['id'], eid))
+
 
         def __enter__(self):
             print('SyncPackage.__enter__')
+            return Package(self._pkg_dict)
 
         def __exit__(self, exc_type, exc_val, exc_tb):
             print('SyncPackage.__exit__')
 
 
-#class Package(collections.MutableMapping):
-#    '''
-#    Wrapper around a CKAN package dict.
-#    '''
-#    def __init__(self, pkg_dict):
-#        self._pkg_dict = pkg_dict
-#
-#    def __getitem__(self, key):
-#        return self._pkg_dict[key]
-#
-#    def __setitem__(self, key, value):
-#        self._pkg_dict[key] = value
-#
-#    def __delitem__(self, key):
-#        del self._pkg_dict[key]
-#
-#    def __iter__(self):
-#        return iter(self._pkg_dict)
-#
-#    def __len__(self):
-#        return len(self._pkg_dict)
+class Package(collections.MutableMapping):
+    '''
+    Wrapper around a CKAN package dict.
+    '''
+    def __init__(self, pkg_dict):
+        self._pkg_dict = pkg_dict
+        self.extras = ExtrasDictView(pkg_dict['extras'])
+
+    def __getitem__(self, key):
+        return self._pkg_dict[key]
+
+    def __setitem__(self, key, value):
+        self._pkg_dict[key] = value
+
+    def __delitem__(self, key):
+        del self._pkg_dict[key]
+
+    def __iter__(self):
+        return iter(self._pkg_dict)
+
+    def __len__(self):
+        return len(self._pkg_dict)
+
+    def __repr__(self):
+        return '<{} {}>'.format(self.__class__.__name__, self['id'])
+
+
+class ExtrasDictView(collections.MutableMapping):
+    '''
+    Wrapper around a CKAN package's "extras".
+    '''
+    def __init__(self, extras):
+        '''
+        Constructor.
+
+        ``extras`` is a list of package extras. That list is managed
+        in-place by the created ``ExtrasDictView`` instance.
+        '''
+        self._extras = extras
+
+    def __getitem__(self, key):
+        for extra in self._extras:
+            if extra['key'] == key:
+                return extra['value']
+        raise KeyError(key)
+
+    def __setitem__(self, key, value):
+        '''
+        Set the value of a package extra.
+
+        If there is an existing extra with the given key its value is
+        replaced. Otherwise, a new extra is appended at the end of the
+        extras list.
+        '''
+        for extra in self._extras:
+            if extra['key'] == key:
+                extra['value'] = value
+                return
+        self._extras.append({'key': key, 'value': value})
+
+    def __delitem__(self, key):
+        for i, extra in enumerate(self._extras):
+            if extra['key'] == key:
+                self._extras.pop(i)
+                return
+        raise KeyError(key)
+
+    def __len__(self):
+        return len(self._extras)
+
+    def __iter__(self):
+        return (extra['key'] for extra in self._extras)
+
 
 
 if __name__ == '__main__':
@@ -94,5 +148,7 @@ if __name__ == '__main__':
     with RemoteCKAN('https://test-transparenz.karlsruhe.de', apikey=apikey) as api:
         imp = Importer(api, 'test-importer', 'stadt-karlsruhe')
         with imp.sync_package('eid1') as pkg:
-            print('OK')
+            print('Package extras:')
+            for extra in pkg.extras:
+                print('  {}'.format(extra))
 
