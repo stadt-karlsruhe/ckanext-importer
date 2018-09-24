14fa620fd5fbd3c7620ee7dc43726a9f5f8eab6c
Florian Brucker <florian.brucker@it.karlsruhe.de>
2018-09-24 14:08:10 +0200

    Build documentation on Travis

--- travis/run.bash
+++ travis/run.bash
@@ -2,5 +2,15 @@
 
 set -e
 
+# Run the tests
 py.test --cov=ckanext.importer ckanext/importer/tests
+
+# Build the documentation
+./make_docs.sh
+
+# Check that CHANGELOG.md contains valid Markdown
+python -m markdown CHANGELOG.md
+
+# Check that README.rst contains valid reStructuredText
+rst2html.py README.rst
 
